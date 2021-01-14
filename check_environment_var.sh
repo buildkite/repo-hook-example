6ef18a847daf87aac151d7c59b95556eecbff442
Chloe Hutchinson <chloe@buildkite.com>
2021-01-14 13:50:13 +1100

    Test

--- check_environment_var.sh
+++ check_environment_var.sh
@@ -2,9 +2,9 @@
 set -eo pipefail
 
 echo "+++ Checking value of an environment variable"
-if [ -z "${GIT_SHORT_HASH}" ]; then
+if [ -z "${TEST}" ]; then
   echo 'ERROR: Variable $GIT_SHORT_HASH is not set!!'
   exit 1
 else
-  echo "\$GIT_SHORT_HASH=$GIT_SHORT_HASH"
+  echo "\$GIT_SHORT_HASH=$TEST"
 fi
